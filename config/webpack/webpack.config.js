dd3cc1a83cb6feca3345c261ed87b03af299ae9b
Kadeem Pardue <kadeempardue@Mac.attlocal.net>
2025-06-14 13:51:00 -0400

    Change bootstrap import

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -3,21 +3,4 @@ const { generateWebpackConfig } = require('shakapacker')
 
 const webpackConfig = generateWebpackConfig()
 
-const sassLoader = {
-  loader: 'sass-loader',
-  options: {
-    sassOptions: {
-      quietDeps: true,
-    },
-  },
-};
-
-
-// Apply the patch
-webpackConfig.module.rules.forEach(rule => {
-  if (rule.test && rule.test.toString().includes('scss')) {
-    rule.use.push(sassLoader)
-  }
-})
-
 module.exports = webpackConfig
